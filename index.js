92387fa2f72c29f340ca4dc04fa3e800cb929996
clyde <amplespencer@pm.me>
2021-05-10 14:59:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Plugin } = require('powercord/entities');
+
+module.exports = class egg extends Plugin {
+  startPlugin() {
+    powercord.api.commands.registerCommand({
+      command: 'egg',
+      description: 'egg',
+      usage: '{c}',
+      executor: () => this.sendEgg()
+    });
+  } 
+
+
+
+  async sendEgg() {
+    return {
+      send: true,
+      result: "\
+       \`\`\`\n\
+       \`.---\`            \n\
+      -/oooooo+-          \n\
+    \`/oooooooooo+-        \n\
+   .oooooooooooooo:       \n\
+  \`oooooooooooooooo:      \n\
+  +ooooooooooooooooo-     \n\
+ -oooooooooooooooooo+     \n\
+ +ooooooooooooooooooo-    \n\
+ oooooooooooooooooooo:    \n\
+ oooooooooooooooooooo:    \n\
+ /ooooooooooooooooooo.    \n\
+ \`+ooooooooooooooooo:     \n\
+  \`+ooooooooooooooo:      \n\
+    -+ooooooooooo/\`       \n\
+      \`.:////:-.          \n\
+      \`\`\`\
+      "
+    };
+  }
+  
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand('egg');
+  }
+};
